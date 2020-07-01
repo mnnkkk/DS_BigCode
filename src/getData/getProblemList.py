9985e41f16c67d98201170ae60b94872df45929a
mnnkkk <46259595+mnnkkk@users.noreply.github.com>
2020-07-01 11:08:44 +0800

    feat: get problem list

---
+++ src/getData/getProblemList.py
@@ -0,0 +1,38 @@
+import json
+
+
+#根据url获取题目名
+def get_problem_name(url: str) -> str:
+    problem_name = url
+    index_of_ = len(problem_name) - 1
+    while problem_name[index_of_] != '_':
+        index_of_ -= 1
+    problem_name = problem_name[57: index_of_]
+    #print(problem_name)
+    return problem_name
+
+
+# 读json
+json_data = open('../../resource/test_data1.json', encoding='utf-8').read()
+data = json.loads(json_data)
+
+json_data = open('../../out/demoOutPut/leetcodeDifficulty.json', encoding='utf-8').read()
+all_problem_list = json.loads(json_data)
+
+# 遍历data
+curr_problem_list = set([])
+for k, v in data.items():
+    for case in v["cases"]:
+        problem_name = get_problem_name(case["case_zip"])
+        #print(problem_name)
+        curr_problem_list.add(problem_name)
+
+res = {}
+ct = 1
+for x in curr_problem_list:
+    res[str(ct)] = x
+    ct += 1
+
+# 写json
+with open("../../resource/test_data1_problem_list.json", "w", encoding="utf-8") as f:
+    json.dump(res, f, ensure_ascii=False)
