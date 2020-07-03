653ffe57fd4204383261ef646c9f2f88d2e8d595
mnnkkk <46259595+mnnkkk@users.noreply.github.com>
2020-07-03 21:30:19 +0800

    feat: get group 5 time cost

--- src/demo/calTimeCost.py
+++ src/demo/calTimeCost.py
@@ -9,15 +9,29 @@
 import json
 
 # 读json
-json_data = open("../../resource/testSample1.json", encoding="utf-8").read()
+json_data = open("../../resource/test_data1_group5.json", encoding="utf-8").read()
 data = json.loads(json_data)
 
 # 基础时间和罚时（根据题目难度）
 # 单位：ms
 # 基础时间为15/30/45min
-# 罚时为二分之一基础时间
-baseTime = 15 * 60 * 1000
-timeUnit = baseTime / 2
+# 罚时为10分钟
+json_data = open("../../out/demoOutPut/leetcodeDifficulty.json", encoding="utf-8").read()
+problems = json.loads(json_data)
+def get_base_time(problem_name: str) ->int:
+    index_of_ = len(problem_name) - 1
+    while problem_name[index_of_] != '_':
+        index_of_ -= 1
+    problem_name = problem_name[57: index_of_]
+    # print(problem_name)
+    diff = 0
+    for k, v in problems.items():
+        if v["name"] == problem_name:
+            diff = v["difficulty"]
+            break
+    return diff * 15 *60 *1000
+
+timeUnit = 10 * 60 * 1000
 
 # 遍历data
 keyVector = []
@@ -32,6 +46,7 @@ for Uid in keyVector:
     for case in data[Uid]["cases"]:
         tryTimes = 0
         timeCost = 0
+        baseTime = get_base_time(case["case_zip"])
 
         for upload in case["upload_records"]:
             tryTimes += 1
@@ -46,5 +61,5 @@ for Uid in keyVector:
 
 
 # 写json
-with open("../../out/demoOutPut/timeCost.json", "w") as f:
+with open("../../out/demoOutPut/time_cost_of_test1_group5.json", "w") as f:
     json.dump(res, f)
