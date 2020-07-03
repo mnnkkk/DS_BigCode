ad034ed32bc549f05503855c996c86df1027c05b
mnnkkk <46259595+mnnkkk@users.noreply.github.com>
2020-07-03 21:13:52 +0800

    feat: get group5 problem list

--- src/getData/getProblemList.py
+++ src/getData/getProblemList.py
@@ -1,19 +1,20 @@
 import json
 
 
-#根据url获取题目名
+# 根据url获取题目名
 def get_problem_name(url: str) -> str:
     problem_name = url
     index_of_ = len(problem_name) - 1
     while problem_name[index_of_] != '_':
         index_of_ -= 1
     problem_name = problem_name[57: index_of_]
-    #print(problem_name)
+    # print(problem_name)
     return problem_name
 
 
 # 读json
-json_data = open('../../resource/test_data1.json', encoding='utf-8').read()
+# json_data = open('../../resource/test_data1.json', encoding='utf-8').read()
+json_data = open('../../resource/test_data1_group5.json', encoding='utf-8').read()
 data = json.loads(json_data)
 
 json_data = open('../../out/demoOutPut/leetcodeDifficulty.json', encoding='utf-8').read()
@@ -24,7 +25,7 @@ curr_problem_list = set([])
 for k, v in data.items():
     for case in v["cases"]:
         problem_name = get_problem_name(case["case_zip"])
-        #print(problem_name)
+        # print(problem_name)
         curr_problem_list.add(problem_name)
 
 res = {}
@@ -34,5 +35,6 @@ for x in curr_problem_list:
     ct += 1
 
 # 写json
-with open("../../resource/test_data1_problem_list.json", "w", encoding="utf-8") as f:
+# with open("../../resource/test_data1_problem_list.json", "w", encoding="utf-8") as f:
+with open("../../resource/test_data1_group5_problem_list.json", "w", encoding="utf-8") as f:
     json.dump(res, f, ensure_ascii=False)
